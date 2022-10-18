7aef5674247855d50c3498d5bfe4273043902de4
chris <chrishethertonweb@gmail.com>
2022-10-18 14:45:05 +0200

    corrections made to NavLink

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,14 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function NavHeader() {
   return (
     <div className="h-[600px] bg-sky-200  mt-[20px]">
       <h1 className=" pt-[20px] text-center text-xl" >About Me</h1>
-      <div className="ml-[20px] flex gap-[50px]" >
-        <NavLink activeclassname='active' to='/' end >Me</NavLink>
-        <NavLink to='/hobbies' >Hobbies</NavLink>
-        <NavLink to='/contact' >Contact</NavLink>
-      </div>
+      <ul className="ml-[20px] flex gap-[50px]" >
+
+        <Link to='/'  ><li>Me</li> </Link>
+        <Link to='/hobbies' > <li>Hobbies</li></Link>
+        <Link to='/contact' ><li>Contact</li></Link>
+      </ul>
     </div>
   );
 }
